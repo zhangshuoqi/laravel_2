65e65719b99d96cfd5e4b31c1c11319df6016e26
FOX <fox@FOX.local>
2018-07-17 18:12:03 +0800

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -44,4 +44,14 @@ class Topic extends Model
         // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 }
